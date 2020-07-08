c35faa6851d0c8386e0c851aabefada66a52e6e1
Fulanull <42985175+fulanull@users.noreply.github.com>
2020-07-08 00:32:02 -0300

    ...

--- python/youtubeDownloader.py
+++ python/youtubeDownloader.py
@@ -2,14 +2,10 @@
 from pytube import YouTube, Stream
 
 
-def show_progress_bar(stream: Stream, chunk: bytes, bytes_remaining: int):
-# def show_progress_bar(stream, chunk, file_handler, bytes_remaining):
-    print("Oi:", bytes_remaining )
-    return
+
 
 def downloadVideo(url, filename = None, filename_prefix = None):
     yt = YouTube(url)
-    yt.register_on_progress_callback(show_progress_bar)
     print(filename_prefix, filename)
     yt.streams.get_highest_resolution().download(output_path="downloaded", filename=filename, filename_prefix=filename_prefix)
 
